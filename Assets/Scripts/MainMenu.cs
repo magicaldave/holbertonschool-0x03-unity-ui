4d402ac8d6afa324296bca30cafe4a20b73b70c9
magicaldave <smokydave@icloud.com>
2023-02-22 11:42:19 -0600

    Add: MainMenu script which initializes the game after menu interaction

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	public void start()
+	{
+	}
+
+	// Init the MAZE GAEM
+	public void PlayMaze()
+	{
+		// Brackeys used the Build Index instead, perhaps faster than iterating through all scenes?
+		// Since this project has two scenes it is at worst, O(2), so no big deal.
+		SceneManager.LoadSceneAsync("maze");
+	}
+
+}
