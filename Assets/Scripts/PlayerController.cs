9af9bf2243f86dbe9afdf5415773e9ab22bbcecd
magicaldave <smokydave@icloud.com>
2023-02-22 00:23:30 -0600

    Edit: Initial commit for valid Accelerometer support. Roll back here if you bust it; clean it later.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,140 +2,166 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+// Install the package, then include this namespace for Gyro
+// For WebGL on iOS, go to Project Settings > Input System Package
+// Enable Motion Usage
+// Make sure to create a settings asset when prompted.
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-	    public float speed = 3.0f;
-	    public float friction = 17.5f;
-	    public Text scoreText;
-	    public Text healthText;
-	    public GameObject endGameContainer;
-	    // This vector is used to store the player's actual velocity
-	    private Vector3 velocity = Vector3.zero;
-	    private int score = 0;
-	    private int health = 5;
+	public float speed;
+	public float friction;
+	public Text scoreText;
+	public Text healthText;
+	public GameObject endGameContainer;
+	private int score = 0;
+	private int health = 5;
+	private Rigidbody rb;
+	// This function pointer will allow us to conditionally determine how to move,
+	// Based on input devices available on the current playform. Bitchin'.
+	public delegate void InputFuncPtr();
+	InputFuncPtr movefunction = null;
 
-	    void Start()
-	    {
-		    // https://docs.unity3d.com/ScriptReference/RuntimePlatform.html
-		    if (Application.platform != RuntimePlatform.WebGLPlayer)
-			    Debug.Log("Hey! You're not using a WebGL build. You should use the Remote to get sensor input.");
-		    else
-		    {
-			    Debug.Log("We'll write some code here later to instantiate the Sensor class.");
-		    }
-	    }
+	void Start()
+	{
+		// https://docs.unity3d.com/ScriptReference/RuntimePlatform.html
+		// Checks for accelerometer support, or an editor being used. Remove the other two options to get your input back.
+		if (SystemInfo.supportsAccelerometer == true)
+		{
+			// Any sensors used must be manually enabled
+			InputSystem.EnableDevice(Accelerometer.current);
+			movefunction = new InputFuncPtr(moveMobile);
+		}
+		else
+			movefunction = new InputFuncPtr(moveKbd);
+		rb = GetComponent<Rigidbody>();
+	}
 
-	    // Update is called once per frame
-	    void Update()
-	    {
-		    // These should be Either 1 or -1. They're floats to support controller or gyro input.
-		    // This is how I check if you're actually moving.
-		    // The transform information is duplicated, with positive or negative values of 1 representing the translation direction
-		    // When magnitude is greater than zero, you pressed a button.
-		    Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		movefunction();
+	}
+
+	void moveKbd()
+	{
+		Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		if (direction != Vector3.zero)
+			rb.AddForce(direction * speed);
+		else
+			rb.velocity *= Mathf.Clamp01(1f - friction * Time.fixedDeltaTime);
+	}
+
+	void moveMobile()
+	{
+		Vector3 tilt, force;
+		float currentTiltMagnitude, currentSpeed, tiltAcceleration, previousTiltMagnitude = 0, speedModFactor = 2.5f;
+		// Read and adjust the device angle
+		tilt = Accelerometer.current.acceleration.ReadValue();
+		tilt = Quaternion.Euler(90, 0, 0) * tilt; // Rotate the tilt vector to match the game's orientation
+		// tilt.Normalize();
 
 
-		    if (direction.magnitude > 0)
-		    {
-			    // Direction * Speed * Time since last fram (Seconds)
-			    velocity += direction * speed * Time.deltaTime;
-			    // Speed limit!
-			    velocity = Vector3.ClampMagnitude(velocity, 20.0f);
-		    }
-		    else
-		    {
-			    // Deceleration is still ongoing.
-			    velocity = velocity.normalized * friction * Time.deltaTime;
-			    if (velocity.magnitude < 0.1f)
-			    {
-				    velocity = Vector3.zero;
-			    }
-		    }
-		    transform.position += velocity * Time.deltaTime;
+		// Track movement between frames
+		currentTiltMagnitude = tilt.magnitude;
+		tiltAcceleration = (currentTiltMagnitude - previousTiltMagnitude) / Time.deltaTime;
+		previousTiltMagnitude = currentTiltMagnitude;
 
-		    // Debug.Log("Accelerometer input: " + Input.acceleration);
-		    transform.position += new Vector3(Input.acceleration.x, 0, 0);
+		// Apply the tilt vector as a force in the X and Z directions, while keeping the player grounded in the Y direction
+		force = new Vector3(tilt.x, 0, tilt.y - 0.707f);
 
-    }
-	    void OnTriggerEnter(Collider other)
-	    {
-		    // Increment score when Player touches object with Pickup tag
-		    // Destroy Pickup on contact
-		    if (other.gameObject.CompareTag("Pickup"))
-		    {
-			    Destroy(other.gameObject);
-			    UpdateTextObj(scoreText, "Score: " + ++score);
-		    }
-		    else if (other.gameObject.CompareTag("Trap"))
-		    {
-			    UpdateTextObj(healthText, "Health: " + --health);
-		    }
-		    else if (other.gameObject.CompareTag("Goal"))
-		    {
-			    EndGameUI("Win");
-		    }
-		    ResetMan();
-	    }
+		if (force.magnitude > 0.08)
+		{
+			currentSpeed = speed + tiltAcceleration * speedModFactor;
+			rb.AddForce(force * currentSpeed);
+			rb.velocity = Vector3.ClampMagnitude(rb.velocity, 10f);
+		}
+		else
+			rb.velocity *= Mathf.Clamp01(1f - friction * Time.fixedDeltaTime);
+	}
 
-	    void ResetMan()
-	    {
-		    if (score == 40)
-		    {
-			    Debug.Log("All Coins Collected!");
-			    score = 0;
-		    }
-		    else if (health == 0)
-		    {
+	void OnTriggerEnter(Collider other)
+	{
+		// Increment score when Player touches object with Pickup tag
+		// Destroy Pickup on contact
+		if (other.gameObject.CompareTag("Pickup"))
+		{
+			Destroy(other.gameObject);
+			UpdateTextObj(scoreText, "Score: " + ++score);
+		}
+		else if (other.gameObject.CompareTag("Trap"))
+		{
+			UpdateTextObj(healthText, "Health: " + --health);
+		}
+		else if (other.gameObject.CompareTag("Goal"))
+		{
+			EndGameUI("Win");
+		}
+		ResetMan();
+	}
 
-			    EndGameUI("Lose");
-			    StartCoroutine(LoadSceneWait(3));
-		    }
-	    }
+	void ResetMan()
+	{
+		if (score == 40)
+		{
+			Debug.Log("All Coins Collected!");
+			score = 0;
+		}
+		else if (health == 0)
+		{
 
-	    IEnumerator LoadSceneWait (int time)
-	    {
-		    yield return new WaitForSeconds(time);
-		    SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
-	    }
+			EndGameUI("Lose");
+			StartCoroutine(LoadSceneWait(3));
+		}
+	}
 
-	    void UpdateTextObj(Component TextObj, string Value)
-	    {
-		    Text UpdateObj = TextObj.GetComponent<Text>();
-		    if (UpdateObj != null)
-		    {
-			    UpdateObj.text = Value;
-		    }
-	    }
+	IEnumerator LoadSceneWait (int time)
+	{
+		yield return new WaitForSeconds(time);
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
 
-	    void EndGameUI(string EndState)
-	    {
-	    // https://docs.unity3d.com/ScriptReference/Color-ctor.html
-		    string EndStateText;
-		    Color TextColor, ContainerColor;
-		    if (EndState.Equals("Win"))
-		    {
-			    EndStateText = "You Win!";
-			    TextColor = new Color(0, 0, 0, 1);
-			    ContainerColor = new Color(0, 1, 0, 1);
+		// Refuse to yield control to the caller until loading is finished
+		while (!asyncLoad.isDone)
+			yield return null;
+	}
 
-		    }
-		    else
-		    {
-			    EndStateText = "Game Over!";
-			    TextColor = new Color(1, 1, 1, 1);
-			    ContainerColor = new Color(1, 0, 0, 1);
-		    }
+	void UpdateTextObj(Component textObject, string newValue)
+	{
+		// Checks type and value, only possible to enter this code if
+		// A text component exists!
+		if (textObject is Text isText)
+			isText.text = newValue;
+	}
 
-		    // Start from the top and work down the hierarchy.
-		    Image endGamePrompt = endGameContainer.GetComponent<Image>();
-		    Text endGameText = endGamePrompt.GetComponentInChildren<Text>();
-		    // Update fields from bottom to top
-		    UpdateTextObj(endGameText, EndStateText);
-		    endGameText.color = TextColor;
-		    endGamePrompt.color = ContainerColor;
-		    // Fire!
-		    // https://docs.unity3d.com/ScriptReference/GameObject.SetActive.html
-		    endGameContainer.SetActive(true);
-	    }
+	void EndGameUI(string EndState)
+	{
+		// https://docs.unity3d.com/ScriptReference/Color-ctor.html
+		string endStateText;
+		Color textColor, containerColor;
+		if (EndState.Equals("Win"))
+		{
+			endStateText = "You Win!";
+			textColor = Color.black;
+			containerColor = Color.green;
+
+		}
+		else
+		{
+			endStateText = "Game Over!";
+			textColor = Color.white;
+			containerColor = Color.red;
+		}
+
+		// Start from the top and work down the hierarchy.
+		Image endGamePrompt = endGameContainer.GetComponent<Image>();
+		Text endGameText = endGamePrompt.GetComponentInChildren<Text>();
+		// Update fields from bottom to top
+		UpdateTextObj(endGameText, endStateText);
+		endGameText.color = textColor;
+		endGamePrompt.color = containerColor;
+		// Fire!
+		// https://docs.unity3d.com/ScriptReference/GameObject.SetActive.html
+		endGameContainer.SetActive(true);
+	}
 }
