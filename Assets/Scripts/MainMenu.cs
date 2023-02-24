a8899a3a0990912cdb673de260d049427e518732
magicaldave <smokydave@icloud.com>
2023-02-23 21:07:15 -0600

    Edit: added QuitMaze function to end the game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,7 +13,14 @@ public class MainMenu : MonoBehaviour
 	{
 		// Brackeys used the Build Index instead, perhaps faster than iterating through all scenes?
 		// Since this project has two scenes it is at worst, O(2), so no big deal.
-		SceneManager.LoadSceneAsync("maze");
+		SceneManager.LoadSceneAsync("Maze");
 	}
 
+	public void QuitMaze()
+	{
+		if (Application.platform.ToString().Contains("Editor"))
+			Debug.Log("Quit Game");
+		else
+			Application.Quit();
+	}
 }
